bf7eff929de12e70ab4a08001d0d8e3764ef3e8e
Ivan <ivanochkoivan2000@gmail.com>
2024-04-23 15:57:33 +0300

    Add project files.

---
+++ UnitTests/OrderServiceTests.cs
@@ -0,0 +1,67 @@
+﻿using DotnetUnitTesting;
+using Moq;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    internal class OrderServiceTests
+    {
+        UserAccount? userJohn;
+        Mock<IDiscountUtility>? mockDiscount;
+        private Product? product1;
+        private Product? product2;
+        [SetUp]
+        public void SetUp() 
+        {
+            userJohn = new UserAccount("John", "Smith", "1990/10/10");
+
+            product1 = new Product(1, "Milk", 20, 1);
+            product2 = new Product(2, "Apple", 10, 1);
+
+            userJohn.ShoppingCart.AddProductToCart(product1);
+            userJohn.ShoppingCart.AddProductToCart(product2);
+
+            mockDiscount = new Mock<IDiscountUtility>();
+            mockDiscount.Setup(x => x.CalculateDiscount(userJohn)).Returns(3);
+        }
+
+        [Test]
+        public void GetOrderPriceForValidUserTest()
+        {
+            OrderService service = new OrderService(mockDiscount!.Object);
+
+            //Act
+            var price = service.GetOrderPrice(userJohn);
+
+            //Assert
+            Assert.That(price, Is.EqualTo(27));
+        }
+
+        [Test]
+        public void GetOrderPriceForValidUserAndOneProductTest()
+        {
+            userJohn!.ShoppingCart.RemoveProductFromCart(product1);
+            OrderService service = new OrderService(mockDiscount!.Object);
+
+            //Act
+            var price = service.GetOrderPrice(userJohn);
+
+            //Assert
+            Assert.That(price, Is.EqualTo(7));
+        }
+
+        [Test]
+        public void GetOrderPriceForNewUserTest()
+        {
+            var user = new UserAccount("Kenny", "Stoch", "1990/10/10");
+            user.ShoppingCart.AddProductToCart(product1);
+            OrderService service = new OrderService(mockDiscount!.Object);
+
+            //Act
+            var price = service.GetOrderPrice(user);
+
+            //Assert
+            Assert.That(price, Is.EqualTo(20));
+        }
+    }
+}
---
+++ UnitTests/ShoppingCartTests.cs
@@ -0,0 +1,77 @@
+﻿using DotnetUnitTesting;
+using NUnit.Framework;
+
+namespace UnitTests
+{
+    internal class ShoppingCartTests
+    {
+        private Product? product1;
+        private Product? product2;
+        private ShoppingCart? shoppingCart;
+
+        [SetUp]
+        public void SetUp() {
+            product1 = new Product(1, "Milk", 20, 1);
+            product2 = new Product(2, "Apple", 10, 1);
+            shoppingCart = new ShoppingCart(new List<Product> { product1, product2 });
+        }
+
+        [Test]
+        public void UserCanAddProductsToTheShoppingCartTest() 
+        { 
+            
+            ShoppingCart cart = new ShoppingCart();
+
+            //Act
+            cart.AddProductToCart(product1);
+            cart.AddProductToCart(product2);
+
+            //Assert
+            Assert.That(cart.GetProductById(product1!.Id), Is.EqualTo(product1));
+            Assert.That(cart.GetProductById(product2!.Id), Is.EqualTo(product2));
+        }
+
+        [Test]
+        public void UserCanRemoveProductsFromTheShoppingCartTest() 
+        {
+            //Act
+            shoppingCart!.RemoveProductFromCart(product1);
+            shoppingCart.RemoveProductFromCart(product2);
+
+            //Assert
+            Assert.That(shoppingCart.Products.Count, Is.EqualTo(0));         
+        }
+
+        [Test]
+        public void UserCanRemovePartOfProductsFromTheShoppingCartTest()
+        {
+            //Act
+            shoppingCart!.RemoveProductFromCart(product1);
+
+            //Assert
+            Assert.That(shoppingCart.Products.Count, Is.EqualTo(1));
+            Assert.That(shoppingCart.GetProductById(product2!.Id), Is.EqualTo(product2));
+        }
+
+        [Test]
+        public void UserCanGetTheTotalPriceOfTheShoppingCartTest()
+        {
+            //Act
+            var price = shoppingCart!.GetCartTotalPrice();
+
+            //Assert
+            Assert.That(price, Is.EqualTo(30));
+        }
+
+        [Test]
+        public void UserCanGetTheTotalPriceOfTheShoppingCartWhereQuantity2Test()
+        {
+            shoppingCart!.AddProductToCart(product1!);
+            //Act
+            var price = shoppingCart!.GetCartTotalPrice();
+
+            //Assert
+            Assert.That(price, Is.EqualTo(50));
+        }
+    }
+}
